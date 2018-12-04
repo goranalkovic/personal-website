1ddf92b9bbc3126de46cd7225f7c351bcd200740
Goran Alković <goran.alkovic@hotmail.com>
2018-12-04 06:38:24 +0100

    Edits

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,24 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getNewImg() {
+    let currentSrc = document.getElementById('navbar-logo').src;
+
+    // console.log(`Current: ${currentSrc.slice(-11)}`);
+
+    let newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`;
+
+    while (newSrc.slice(-11) === currentSrc.slice(-11)) {
+        newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`
+    }
+
+    // console.log(`New: ${newSrc.slice(-11)}`);
+
+    return newSrc;
+}
+
 function swapNavLogo() {
 
-    $('#navbar-logo').fadeOut(300, () => {
-        let currentSrc = document.getElementById('navbar-logo').src;
-
-        console.log(`Current: ${currentSrc.slice(-11)}`);
-
-        let newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`;
-
-        while (newSrc.slice(-11) === currentSrc.slice(-11)) {
-            newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`
-        }
-
-        console.log(`New: ${newSrc.slice(-11)}`);
-
-        document.getElementById('navbar-logo').src = newSrc;
-
-        $('#navbar-logo').fadeIn(200);
-    })
-
+    $('#navbar-logo').hide().attr('src', getNewImg()).show();
 
 }
