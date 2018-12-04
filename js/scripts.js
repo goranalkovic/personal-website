4b84c0ea0a3cc411619e6bc78d9b5dd204d8b5b1
Goran Alković <goran.alkovic@hotmail.com>
2018-12-04 06:32:27 +0100

    Edits

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,15 @@ function getRandomInt(min, max) {
 function swapNavLogo() {
     let currentSrc = document.getElementById('navbar-logo').src;
 
-    console.log(`Current: ${currentSrc}`);
+    console.log(`Current: ${currentSrc.slice(-11)}`);
 
     let newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`;
 
-    while (newSrc === currentSrc) {
+    while (newSrc.slice(-11) === currentSrc.slice(-11)) {
         newSrc = `img/logo/logo-v${getRandomInt(1, 9)}.svg`
     }
 
-    console.log(`New: ${newSrc}`);
+    console.log(`New: ${newSrc.slice(-11)}`);
 
     document.getElementById('navbar-logo').src = newSrc;
 }
