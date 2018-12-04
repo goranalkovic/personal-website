85bbee4f7790ed6ee2062769ce5fc2f7ebbea044
Goran Alković <goran.alkovic@hotmail.com>
2018-12-04 06:59:12 +0100

    Edits

--- js/scripts.js
+++ js/scripts.js
@@ -28,10 +28,35 @@ function getNewImg() {
 
 function swapNavLogo() {
 
-    $('#navbar-logo').fadeOut("slow");
-
-    setTimeout(() => { $('#navbar-logo').attr('src', getNewImg()); }, 400);
-
-    setTimeout(() => { $('#navbar-logo').fadeOut("slow"); }, 800);
+    $('#navbar-logo').attr('src', getNewImg()).animateCss('jello');
 
 }
+
+// Animate.css extension method
+
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        let animationEnd = (function (el) {
+            let animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (let t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+        })(document.createElement('div'));
+
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    },
+});
