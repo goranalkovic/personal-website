4783679e9efbbf404a152ed44f8f6771fe378351
Goran Alković <goran.alkovic@hotmail.com>
2018-12-04 05:56:11 +0100

    Edits

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,9 @@ function getRandomInt(min, max) {
 
 function switchNavbarImg() {
 
-    let imageSrc = `img/logo/logo-v${getRandomInt(1,9)}.svg`;
-
-    $("#navbar-logo").attr("src", imageSrc);
+    $("#navbar-logo")
+        .fadeTo("slow", 0.0)
+        .attr("src", `img/logo/logo-v${getRandomInt(1,9)}.svg`)
+        .fadeTo("slow", 1.0);
 
 }
