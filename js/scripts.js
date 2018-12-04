c2c1bdb7588ca04060e22f4cfdc6a0670df7fd30
Goran Alković <goran.alkovic@hotmail.com>
2018-12-04 06:13:02 +0100

    Edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    document.getElementById('navbar-img-link').addEventListener('click', () => {
-        document.getElementById('navbar-logo').src = `img/logo/logo-v${getRandomInt(1,9)}.svg`;
-    });
-
     console.log("Loaded");
+
+    swapNavLogo();
 });
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function swapNavLogo() {
+    document.getElementById('navbar-logo').src = `img/logo/logo-v${getRandomInt(1,9)}.svg`;
 }
