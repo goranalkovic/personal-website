0c068eea7fe9eb5142f6c1a456a1a612c22c47e0
Goran Alković <goran.alkovic@hotmail.com>
2019-05-29 14:23:11 +0200

    JSON project loading implemented

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
     loadProjects();
 });
 
+window.addEventListener("scroll",function(){
+    let windowHeight = document.documentElement.clientHeight;
+
+    if (window.pageYOffset >= windowHeight + windowHeight * 0.05 ) {
+        document.querySelector('.navbar').classList.add('navbar-shadow');
+    } else {
+        document.querySelector('.navbar').classList.remove('navbar-shadow');
+    }
+});
+
 let usedLogos = [1, 4, 7, 2, 5, 8, 3, 6, 9];
 
 function getRandomElement() {
@@ -80,7 +90,9 @@ function swapNavLogo() {
     let logo = document.querySelector('#navbar-logo');
 
     // Start animation
-    logo.classList.add('animated shake fast');
+    logo.classList.add('animated');
+    logo.classList.add('shake');
+    logo.classList.add('fast');
 
     // Swap logo
     setTimeout(() => {
@@ -89,14 +101,204 @@ function swapNavLogo() {
 
     // Remove animation
     setTimeout(() => {
-        logo.classList.remove('animated shake fast');
+        logo.classList.remove('animated');
+        logo.classList.remove('shake');
+        logo.classList.remove('fast');
         canSwap = true;
     }, 850);
 
 }
 
-function loadProjects(){
-    fetch('https://goranalkovic.github.io/files/projects.json')
-        .then(response => response.json())
-        .then(json => console.log(json));
+function closeModal() {
+    document.querySelector('#gallery-modal').classList.add('is-modal-hidden');
+    setTimeout(() =>  {
+        document.querySelector('#gallery-modal').classList.remove('is-active');
+    }, 300);
+}
+
+function showModal() {
+    document.querySelector('#gallery-modal img').setAttribute('src', currentProject.images[0]);
+
+    document.querySelector('#gallery-modal').classList.add('is-active');
+    setTimeout(() =>  {
+        document.querySelector('#gallery-modal').classList.remove('is-modal-hidden');
+    }, 300);
+
+}
+
+function nextImage(){
+    if (currentImage + 1 <= currentProject.images.length - 1) {
+        currentImage++;
+    }
+    else {
+        currentImage = 0;
+    }
+
+    document.querySelector('#gallery-modal img').setAttribute('src', currentProject[currentImage]);
+}
+
+function prevImage() {
+    if (currentImage - 1 >= 0) {
+        currentImage--;
+    }
+    else {
+        currentImage = currentProject.images.length - 1;
+    }
+
+    document.querySelector('#gallery-modal img').setAttribute('src', currentProject[currentImage]);
+
+}
+
+async function loadProjects() {
+    let data = [
+        {
+            "name": "St@k",
+            "yearStart": 2016,
+            "yearEnd": null,
+            "description": "Print design for the student magazines St@k and St@kić",
+            "heroImage": "https://goranalkovic.github.io/img/projects/stak/card-hero.png",
+            "images": [
+                "https://goranalkovic.github.io/img/projects/stak/hero-1.png",
+                "https://goranalkovic.github.io/img/projects/stak/hero-2.png",
+                "https://goranalkovic.github.io/img/projects/stak/hero-3.png",
+                "https://goranalkovic.github.io/img/projects/stak/hero-4.png",
+            ],
+            "tags": [
+                {
+                    "text": "Print design",
+                    "color": "is-primary"
+                },
+                {
+                    "text": "InDesign",
+                    "color": ""
+                },
+                {
+                    "text": "Illustrator",
+                    "color": ""
+                },
+                {
+                    "text": "Photoshop",
+                    "color": ""
+                }
+            ]
+        }
+    ];
+
+    let response = await fetch('/files/projects.json');
+    data = await response.json();
+
+    for (let project of data) {
+       addProjectCard(project, document.querySelector('#project-container'));
+    }
+}
+
+var currentProject;
+var currentImage = 0;
+
+function addProjectCard(project, projectContainer) {
+
+
+    // Container div
+    let containerDiv = document.createElement('div');
+    containerDiv.className = 'column is-narrow is-12-mobile is-6-tablet is-4-desktop is-4-widescreen';
+
+    // Card
+
+    let cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+
+    // Card header image
+
+    let cardImgDiv = document.createElement('div');
+    cardImgDiv.className = 'card-image';
+
+    let cardImgFigure = document.createElement('figure');
+    cardImgFigure.className = 'image is-3by2';
+
+    let cardImgImage = document.createElement('img');
+    cardImgImage.setAttribute('src', project.heroImage);
+    cardImgImage.setAttribute('alt', 'Project image');
+
+    cardImgFigure.appendChild(cardImgImage);
+    cardImgDiv.appendChild(cardImgFigure);
+
+    // Card contents
+
+    let cardContentDiv = document.createElement('div');
+    cardContentDiv.className = 'card-content';
+
+    // Card title
+
+    let cardTitleDiv = document.createElement('div');
+    cardTitleDiv.className = 'columns is-mobile';
+
+    let cardTitleColumn1Div = document.createElement('div');
+    cardTitleColumn1Div.className = 'column';
+
+    let projectTitle = document.createElement('h4');
+    projectTitle.className = 'title is-4';
+    projectTitle.innerHTML = project.name;
+
+    let projectDuration = document.createElement('h6');
+    projectDuration.className = 'subtitle is-6';
+    projectDuration.innerHTML = `${project.yearStart}-${project.yearEnd == null ? '' : project.yearEnd}`;
+
+    cardTitleColumn1Div.appendChild(projectTitle);
+    cardTitleColumn1Div.appendChild(projectDuration);
+
+    let cardTitleColumn2Div = document.createElement('div');
+    cardTitleColumn2Div.className = 'column is-narrow';
+
+    let galleryBtn = document.createElement('button');
+    galleryBtn.className = 'button is-rounded';
+    galleryBtn.addEventListener('click', () => {
+        currentProject = project;
+        showModal();
+    });
+
+    let galleryBtnSpan = document.createElement('span');
+    galleryBtnSpan.className = 'icon is-medium';
+
+    let galleryBtnIcon = document.createElement('i');
+    galleryBtnIcon.className = 'icon-picture';
+
+    galleryBtnSpan.appendChild(galleryBtnIcon);
+    galleryBtn.appendChild(galleryBtnSpan);
+    cardTitleColumn2Div.appendChild(galleryBtn);
+
+    cardTitleDiv.appendChild(cardTitleColumn1Div);
+    cardTitleDiv.appendChild(cardTitleColumn2Div);
+
+    // Card body
+
+    let projectDescription = document.createElement('div');
+    projectDescription.className = 'content';
+    projectDescription.innerHTML = project.description;
+
+    // Tags
+
+    let tags = document.createElement('div');
+    tags.className = 'tags';
+
+    for (let tag of project.tags) {
+        let newTag = document.createElement('span');
+        newTag.className = 'tag is-rounded ' + tag.color;
+        newTag.innerHTML = tag.text;
+        tags.appendChild(newTag);
+    }
+
+    cardContentDiv.appendChild(cardTitleDiv);
+    cardContentDiv.appendChild(projectDescription);
+    cardContentDiv.appendChild(tags);
+
+    //
+
+    cardDiv.appendChild(cardImgDiv);
+    cardDiv.appendChild(cardContentDiv);
+
+    //
+
+    containerDiv.appendChild(cardDiv);
+
+    projectContainer.appendChild(containerDiv);
 }
